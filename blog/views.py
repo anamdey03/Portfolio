e86731240e0f2438d8c85cab7a7433f6a3b03fe5
Anamitra Dey <anamitradey.ece@gmail.com>
2020-12-10 12:57:45 -0500

    blog functionality and application running

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
 
 # Create your views here.
+def allblogs(request):
+    blogs = Blog.objects
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'blog/allblogs.html', context)
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    context = {
+        'blog': blog
+    }
+    return render(request, 'blog/detail.html', context)
