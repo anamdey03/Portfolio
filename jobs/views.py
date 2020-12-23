8f50e2efed12a8f7adb386b67e5abb1a5ba8c6bd
Anamitra Dey <anamitradey.ece@gmail.com>
2020-12-23 14:51:25 -0500

    Email Sending Functionality

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Job
+from django.core.mail import send_mail
+from portfolio.settings import EMAIL_HOST_USER
 
 
 # Create your views here.
@@ -16,4 +18,23 @@ def about(request):
 
 
 def contact(request):
-    return render(request, 'jobs/contact.html', {})
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        phone_number = request.POST['phone']
+        comments = request.POST['comments']
+
+        send_mail(
+            'Comments from ' + name + ' for ForeverMyWanderlust',
+            comments,
+            EMAIL_HOST_USER,
+            ['anamitradey.ece@gmail.com', 'sagarikapaul6@gmail.com', email]
+        )
+
+        context = {
+            'name': name
+        }
+
+        return render(request, 'jobs/contact.html', context)
+    else:
+        return render(request, 'jobs/contact.html', {})
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,6 +137,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
 
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'forevermywanderlust@gmail.com'
+EMAIL_HOST_PASSWORD = 'brbicttsrphulbaa'
+EMAIL_USE_TLS = True
+
 try:
     from .local_settings import *
 except ImportError:
